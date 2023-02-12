6470d6158ee7ec1b4f08a8f709b9c33f5bd839f0
Sandro Garani <sandro.garani@outlook.com.br>
2023-02-12 18:27:08 +0000

    first commit

---
+++ README.md
@@ -0,0 +1,9 @@
+test_dev
+=====
+
+An OTP application
+
+Build
+-----
+
+    $ rebar3 compile
---
+++ apps/test_dev/src/test_dev_app.erl
@@ -0,0 +1,18 @@
+%%%-------------------------------------------------------------------
+%% @doc test_dev public API
+%% @end
+%%%-------------------------------------------------------------------
+
+-module(test_dev_app).
+
+-behaviour(application).
+
+-export([start/2, stop/1]).
+
+start(_StartType, _StartArgs) ->
+    test_dev_sup:start_link().
+
+stop(_State) ->
+    ok.
+
+%% internal functions
